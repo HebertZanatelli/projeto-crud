374c4a212b4fab269584ddb185f656c69e4c0418
Hebert Zanatelli <hebert_alves1@hotmail.com>
2023-04-04 18:50:40 -0300

    adicionando funcao de criptografar senhas

--- src/controllers/customers.js
+++ src/controllers/customers.js
@@ -1,18 +1,21 @@
 const CustomerModel = require('../models/customers')
+const {crypto} = require('../utils/password')
 
-function add(req, res) {
+async function add(req, res) {
     const {
         name,
         age,
         email,
         password,
     } = req.body
+
+    const passwordCrypto = await crypto(password)
     
     const register = new CustomerModel({
         name,
         age,
         email,
-        password
+        password: passwordCrypto
     })
     
     register.save()
